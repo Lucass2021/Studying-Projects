197221c714782dc0eed4bf524519f46c70bd306b
Lucas Dias <lucas.allx@hotmail.com>
2021-02-25 20:18:40 -0300

    Review content

--- Aulas JavasScript2/main.js
+++ Aulas JavasScript2/main.js
@@ -1,1 +1,25 @@
-9, 46 revisw
+//For Loop and While Loop--------------------------------------------------------
+
+const names = ["Ed", "John", "Maria", "Eliza", "Burrito", "Harry", "Potter"];
+
+for (name of names) {
+    console.log(`Hello there ${name}`);
+}
+
+
+let loading = 0;
+
+while (loading < 100) {
+    console.log("Website is loading");
+
+    loading++;
+}
+
+//Dom manipulation---------------------------------------------
+
+//Acess element in HTML
+const text = document.querySelector(".title");
+const button = document.querySelector(".change-color");
+
+text.style.color = "red";
+button.style.background = "purple";
